cf205d9c156b3bef9c599f0fb03e16155c5611f6
tony555t <your-email@example.com>
2025-07-10 17:25:09 +0300

    components

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+const UserProfile = ()=>{
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <p>Age:{props.Age}</p>
+            <p>Bio:{props.bio}</p>
+        </div>
+    )
+}
