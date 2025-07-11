1c6c3d8ccca051d0f86d4dfd2acf3f3079d420f1
tony555t <your-email@example.com>
2025-07-11 12:31:47 +0300

    file structure

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import MainContent from './MainContent';
+import Header from './Header';
+import MainContent from '../MainContent';
 import Footer from './Footer';
 
 function App() {
