93252a15ad308785309fdfb60d53752aaab47191
tony555t <your-email@example.com>
2025-07-11 11:51:17 +0300

    comments

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,9 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone , am learning React at ALX</h1>
-            <p> I am learning about jsx </p>
-            <p> iam learning about jsx!</p>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
