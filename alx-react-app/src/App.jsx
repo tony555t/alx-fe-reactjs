1898a248a82925d41b3cbba9998271078e1631fe
tony555t <your-email@example.com>
2025-07-14 10:44:34 +0300

    file structure

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <WelcomeMessage />
       <Footer />
     </div>
   );
