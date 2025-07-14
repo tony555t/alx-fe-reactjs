46c55c8314f99d6bc3e46875579315d803b5d726
tony555t <your-email@example.com>
2025-07-14 15:32:53 +0300

    context implimentation

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,37 +1,19 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './components/UserProfile';
-import Counter from './components/counter';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-      <Footer />
-      <Counter />
-    </div>
-  );
-}
+import ProfilePage from './ProfilePage';
+import UserContext from './components/UserContext';
+
+function App(){
+  const UserData = {name: "jane doe" ,email:"tonytullah764@gmail.com"};
+};
+
+  return(
+    <useContext.provider value={UserData}>
+      <div>
+        <ProfilePage />
+      </div>
+    </useContext.provider>
+
+  )
+
 
 export default App;
-
-
-
-// import React from 'react';
-// import UserProfile from './components/UserProfile';
-
-
-// function App() {
-//   return (
-//     <div>
-//       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-//     </div>
-//   );
-// }
-
-// export default App;
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,30 @@
+import React ,{ useContext} from 'react';
+import UserContext from '../UserContext';
+
+
+
+
+
+
+function UserDetails() {
+const userData = useContext=(UserContext);
+    
+    return (
+      <div style={{
+        border: '1px solid #ccc',
+        padding: '20px',
+        borderRadius: '8px',
+        backgroundColor: '#f9f9f9',
+        maxWidth: '400px',
+        margin: '20px auto'
+      }}>
+        <h3>user Details</h3>
+        <p><strong>Name:</strong> {userData.name}</p>
+        <p><strong>Email:</strong> {userData.email}</p>
+
+     
+      </div>
+    );
+  }
+  
+  export default UserDetails;
