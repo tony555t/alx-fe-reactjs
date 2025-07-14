959b6043739779f4ef92104198514107b7f2b00f
tony555t <your-email@example.com>
2025-07-14 15:57:44 +0300

    context implimentation

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,4 +1,5 @@
-import React ,{createContext} from 'react';
- const  UserContext= createContext();
- 
- export default UserContext;
+import React, { createContext } from 'react';
+
+const UserContext = createContext();
+
+export default UserContext;
