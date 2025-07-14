d505a6a19849c8fd8a99e7144ad14664752300b5
tony555t <your-email@example.com>
2025-07-14 13:52:31 +0300

    new styles

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,20 @@
 function Header() {
     return (
-      <header>
-        <h1>My Favorite Cities</h1>
+      <header style={{
+        background:'navy',
+        color:'white',
+        textAlign:'center',
+        padding:'20px',
+          boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+          marginBottom: '20px'
+
+      }}>
+        <h1 style ={{
+             margin: '0',
+             fontSize: '2.5rem',
+             fontWeight: 'bold',
+             letterSpacing: '1px'
+        }}>My Favorite Cities</h1>
       </header>
     );
   }
