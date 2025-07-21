94c9fe16c841dc7d5ff51186675ebb751096d192
tony555t <your-email@example.com>
2025-07-21 14:33:23 +0300

    Update App.jsx with routing

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,35 +1,14 @@
-import React from 'react';
-import { BrowserRouter as  Router,Routes ,Route} from "react-router-dom";
-
-import UserContext from './components/UserContext';
 import ProfilePage from './components/ProfilePage';
-import Navbar from './components/Navbar';
-import Home from '../../my-company/Home';
-import About from '../../my-company/About';
-import Service from '../../my-company/Services';
-import Contact from '../../my-company/Contact';
-
-
-
-
-
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile " element={<ProfilePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
-        
-
-      </Routes>
-      </Router>
+      <div>
+        <ProfilePage />
+      </div>
     </UserContext.Provider>
   );
 }
