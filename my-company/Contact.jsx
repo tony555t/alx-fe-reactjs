26e962e115c5ad42b6503227bc4df0ca4b9aa033
tony555t <your-email@example.com>
2025-07-21 12:58:23 +0300

    more components

---
+++ my-company/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to our homepage!</p>
+    </div>
+  );
+};
+
+export default Contact;
