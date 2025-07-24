c5132a6e72956ee5d2386ade01b19a72bf02b055
tony555t <your-email@example.com>
2025-07-24 11:40:50 +0300

    recipe detail components

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,1 +1,18 @@
-impo
+import  {useRecipeStore } from '../recipeStore'
+
+const DeleteRecipeButton =({ recepeId})=>{
+    const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+
+    const handleDelete=()=>{}
+        if (window.confirm('Are you sureyou wnt to delete this recipe?')){
+            deleteRecipe(recepeId);
+        }
+    
+};
+return(
+    <button onClick={handleDelete} style ={{backgroundColor:'red',color:'white'}}>
+        delete recipe
+    </button>
+)
+
+export default DeleteRecipeButton
