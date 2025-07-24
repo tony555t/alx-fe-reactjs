f478e943fb73f070d38948b6ac7b361ce80ea6c9
tony555t <your-email@example.com>
2025-07-24 15:12:54 +0300

    recipe detele components

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -2,10 +2,35 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm:'',
+  filteredRecipes: [],
+
   addRecipe: (newRecipe) => set((state) => ({ 
     recipes: [...state.recipes, newRecipe] 
   })),
   setRecipes: (recipes) => set({ recipes }),
+
+  
+  setSearchTerm: (term) =>
+    set((state) => {
+      const lowerTerm = term.toLowerCase();
+      const filtered = state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(lowerTerm)
+      );
+      return { searchTerm: term, filteredRecipes: filtered };
+    }),
+
+    filterRecipes: () =>
+        set((state) => ({
+          filteredRecipes: state.recipes.filter((recipe) =>
+            recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+          ),
+        })),
+
+
+
+
+
   deleteRecipe: (id) => set((state) => ({
     recipes: state.recipes.filter(recipe => recipe.id !== id)
   })),
