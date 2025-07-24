924075d2a1b3f2a35aeffeec1aace4fa8e897ff7
tony555t <your-email@example.com>
2025-07-24 11:25:05 +0300

    updating store

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,9 +1,17 @@
 import { create } from 'zustand';
 
- const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ 
     recipes: [...state.recipes, newRecipe] 
   })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  }))
 }));
