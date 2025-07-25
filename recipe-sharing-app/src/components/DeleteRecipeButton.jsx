4e8728e2e67fd2488330dfd3e10601335b8c7f45
tony555t <your-email@example.com>
2025-07-25 12:42:39 +0300

    navigation

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,9 +1,10 @@
-
+import {useNavigate} from 'react-router-dom'
 import { useRecipeStore } from '../recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-
+  const navigate  = useNavigate();
+  
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete this recipe?')) {
       deleteRecipe(recipeId);
