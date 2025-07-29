daff3962a6d90877d58437469b0bbaa3b24b6771
tony555t <your-email@example.com>
2025-07-29 10:59:25 +0300

    test

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
       const data = await fetchUserData(inputValue);
       setUserData(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
