86695dede800c6265c20467c34410ae6f7f12163
tony555t <your-email@example.com>
2025-08-07 14:04:10 +0300

    new changes

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,12 +1,6 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ✅ Add this
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-        react(),
-        tailwindcss(), // ✅ Add this
-  ],
+  plugins: [react()],
 })
