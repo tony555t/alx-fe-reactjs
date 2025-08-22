ec3556531eafaabec6f10bce5414a5b63b2bf2b4
tony555t <your-email@example.com>
2025-08-22 12:45:34 +0300

    controled form

---
+++ form-handling-react/src/ControlledForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function ControlledForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default ControlledForm;
